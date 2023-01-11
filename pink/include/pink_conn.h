f369e5e8f6171bd9c94f558c44b29188bfb437bd
Salvatore Dipietro <dipiets@amazon.com>
2023-01-11 09:27:43 -0800

    compiling on aarch64

--- pink/include/pink_conn.h
+++ pink/include/pink_conn.h
@@ -118,7 +118,7 @@ class PinkConn : public std::enable_shared_from_this<PinkConn> {
   std::string ip_port_;
   bool is_reply_;
   bool is_writable_;
-  bool close_;
+  bool close_{false};
   struct timeval last_interaction_;
   int flags_;
 
